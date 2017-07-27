a99672250bea7335ada93bd65c15a95199617fcf
sarah schaab <sarahschaab@me.com>
2017-07-27 14:36:28 -0400

    pans check

--- db/migrate/20170727112312_create_comments.rb
+++ db/migrate/20170727112312_create_comments.rb
@@ -2,9 +2,9 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.string :comment_string, null: false
-      t.references :commentable_id, index: true
-      t.references :commentable_type, index: true
-      t.references :commenter_id, index: true
+      t.references :commentable, polymorphic: true, index: true
+      # t.references :commentable_type, index: true
+      t.references :commenter, index: true
 
       t.timestamps
     end
