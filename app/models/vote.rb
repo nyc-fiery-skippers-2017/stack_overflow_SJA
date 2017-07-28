beb5dae7bcbda38cc3aa31c7671d83f0e4b7fcb6
Akash Antony <akashantony23@gmail.com>
2017-07-28 13:52:05 -0400

    made changes to model user

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   # Remember to create a migration!
-  belongs_to :voter, class_name: "User"
+  belongs_to :user
   belongs_to :voteable, polymorphic: true
 
   validates :vote_choice, {presence: true}#not sure what to do here
