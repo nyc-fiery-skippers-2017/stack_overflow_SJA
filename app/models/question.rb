6d52fca07c20beade6f0a5ffb17dd025e47cff28
sarah schaab <sarahschaab@me.com>
2017-07-28 10:42:28 -0400

    fix db bugs

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,10 @@
 class Question < ApplicationRecord
   # Remember to create a migration!
   belongs_to :user
+  has_many :answers 
   has_many :comments, as: :commentable
   has_many :votes, as: :voteable
+
 
   validates :title, :body, {presence: true}
 end
--- db/migrate/20170727112305_create_answers.rb
+++ db/migrate/20170727112305_create_answers.rb
@@ -2,7 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
       t.string :answer_string, null: false
-      t.references :responder, index: true 
+      t.references :responder, index: true
+      t.references :question, index: true
 
       t.timestamps
     end
