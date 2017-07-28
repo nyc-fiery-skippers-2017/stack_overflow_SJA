856c82ae55bc52da00db07075131485ae3170580
sarah schaab <sarahschaab@me.com>
2017-07-28 15:08:11 -0400

    changes

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -3,11 +3,30 @@ get '/questions' do
     erb :'questions/index'
 end
 
+get 'questions/new' do
+  erb :'questions/new'
+end
 
 get '/questions/:id' do
-  p params
-  # @user = User.find(params[:id])
   @question = Question.find(params[:id]) #define instance variable for view
   erb :'questions/show' #show single user view
+end
 
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions"
+  else
+    @errors = @question.errors.full_messages
+    erb :'/questions/new'
+  end
+end
+
+delete '/questions/:id' do
+  question = Question.find_by(id: params[:id])
+  if question
+    question.destroy
+    redirect '/questions'
+  end
 end
